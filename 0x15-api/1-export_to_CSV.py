36cc8d8e2cd2ace0a63bb737a2f9d86fdd4ec15d
kostum-kirakos <lkoustim.mohamed@@yahoo.com>
2024-01-29 08:20:46 -0800

    Committing a script that exports the info retreived using REST API to a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
+"""
+exports employee info to csv
+"""
+if __name__ == "__main__":
+    import csv
+    import requests
+    import json
+    from sys import argv
+
+    USER_ID = argv[1]
+    request = requests.get(f"https://jsonplaceholder.typicode.com/users/{USER_ID}")
+    USERNAME = request.json()["username"]
+    all_tasks = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos?userId={USER_ID}"
+            )
+    with open(f"{USER_ID}.csv", "w", encoding="UTF-8") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for dict in all_tasks.json():
+            data = [
+                    str(USER_ID),
+                    str(USERNAME),
+                    str(dict.get("completed")),
+                    str(dict.get("title")),
+                    ]
+            writer.writerow(data)
 
